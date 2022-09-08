af0abdf3dd61613814c129837ea24bde69b30ce2
yveshield <yveshield@gmail.com>
2022-09-08 08:34:56 +0800

    build dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
